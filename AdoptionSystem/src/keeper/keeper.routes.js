d2800a890ebbc840cd8bd19ad9e2b376ecd4e043
jrealiquez-2019342 <jrealiquez-2019342@kinal.edu.gt>
2024-02-26 18:46:37 -0600

    files deleted

--- AdoptionSystem/src/keeper/keeper.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { registerKeeper } from "./keeper.controller.js";
-
-const api = express.Router();
-
-
-api.post('/registerKeeper', registerKeeper);
-
-export default api;
